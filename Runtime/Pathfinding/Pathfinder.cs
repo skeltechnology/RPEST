ac0c785b2ca9018490a9d4b537db7e6241474647
Skel Tech <skeltechnology@gmail.com>
2022-07-13 12:38:02 +0100

    Added documentation to Pathfinding

--- Runtime/Pathfinding/Pathfinder.cs
+++ Runtime/Pathfinding/Pathfinder.cs
@@ -6,13 +6,27 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 namespace SkelTech.RPEST.Pathfinding {
+    /// <summary>
+    /// Class that manages the pathfinding of a tilemap.
+    /// </summary>
     public class Pathfinder {
         #region Fields
+        /// <summary>
+        /// Grid representation.
+        /// </summary>
         private Cell[,] grid;
+
+        /// <summary>
+        /// Reference to the shortest path solver.
+        /// </summary>
         private ShortestPathSolver solver;
         #endregion
 
         #region Constructors
+        /// <summary>
+        /// Constructor of the class.
+        /// </summary>
+        /// <param name="tilemap">Tilemap to be searched.</param>
         public Pathfinder(Tilemap tilemap) {
             this.grid = Pathfinder.GenerateGrid(tilemap);
             this.solver = new ShortestPathSolver(grid);
@@ -20,6 +34,14 @@ namespace SkelTech.RPEST.Pathfinding {
         #endregion
 
         #region Operators
+        /// <summary>
+        /// Finds the shortest path of the two given positions, taking into account the given obstacles.
+        /// </summary>
+        /// <param name="startGridPosition">Start position of the path (grid coordinates).</param>
+        /// <param name="endGridPosition">End position of the path (grid coordinates).</param>
+        /// <param name="obstacles">Collection of positions the represent obstacles (grid coordinates).</param>
+        /// <param name="maxIterations">Maximum number of iterations that the algorithm will go through.</param>
+        /// <returns></returns>
         public Path FindShortestPath(Vector3Int startGridPosition, Vector3Int endGridPosition, ICollection<Vector3Int> obstacles, int maxIterations) {
             if (!this.IsValidPath(startGridPosition, endGridPosition)) return null;
 
@@ -38,6 +60,11 @@ namespace SkelTech.RPEST.Pathfinding {
         #endregion
 
         #region Initialization
+        /// <summary>
+        /// Generates the grid representation according to the given tilemap.
+        /// </summary>
+        /// <param name="tilemap">Tilemap that will be used to generate the grid.</param>
+        /// <returns>Grid representation of the tilemap.</returns>
         private static Cell[,] GenerateGrid(Tilemap tilemap) {
             tilemap.CompressBounds();
 
@@ -55,12 +82,19 @@ namespace SkelTech.RPEST.Pathfinding {
             return grid;
         }
 
+        /// <summary>
+        /// Resets every cell of the grid.
+        /// </summary>
         private void ResetGrid() {
             foreach (Cell cell in this.grid) {
                 cell?.Reset();
             }
         }
 
+        /// <summary>
+        /// Converts the collection of obstacles to the grid representation.
+        /// </summary>
+        /// <param name="obstacles">Collection of positions that represents the obstacles.</param>
         private void InitializeObstacles(ICollection<Vector3Int> obstacles) {
             if (obstacles == null) return;
             
@@ -74,6 +108,12 @@ namespace SkelTech.RPEST.Pathfinding {
         #endregion
 
         #region Helpers
+        /// <summary>
+        /// Indicates if the given path finding is valid (valid start and end positions).
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
         private bool IsValidPath(Vector3Int start, Vector3Int end) {
             if (start == null || end == null) return false;
             if (!this.solver.IsValidPosition(start.y, start.x)) return false;
